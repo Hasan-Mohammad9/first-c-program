7d654d94465325075cb6038038cc541b12e3169b
Hasan-Mohammad9 <hasanfd0289@gmail.com>
2024-08-26 14:05:54 +0600

    game with C

--- programming_c.c
+++ programming_c.c
@@ -1,16 +1,33 @@
 #include <stdio.h>
-#include <math.h>
+#include <stdlib.h>
+#include <time.h>
 
-int main () {
-    double a = 3.1416, b = 9.8, result;
-    result = pow (a , b);
-    printf("The result is : %lf \n", result);
-    printf("Ceit of result is : %lf \n", ceil(result));
-    printf("Floor of result is : %lf \n", floor(result));
-    printf("Trunc of resut is : %lf \n", trunc(result));
-
+int main() {
+    int number, guess, attempts = 0;
     
+    // Initialize random seed
+    srand(time(0));
+    // Generate a random number between 1 and 100
+    number = rand() % 100 + 1;
     
+    printf("Welcome to the Number Guessing Game!\n");
+    printf("I have chosen a number between 1 and 100.\n");
+    printf("Can you guess what it is?\n\n");
 
-    return (0);
+    // Game loop
+    do {
+        printf("Enter your guess: ");
+        scanf("%d", &guess);
+        attempts++;
+
+        if(guess > number) {
+            printf("Too high! Try again.\n\n");
+        } else if(guess < number) {
+            printf("Too low! Try again.\n\n");
+        } else {
+            printf("Congratulations! You guessed the number in %d attempts.\n", attempts);
+        }
+    } while(guess != number);
+
+    return 0;
 }
